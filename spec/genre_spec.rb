a15cbb28fd2938bd2774fbd3339642a1953a8207
Enoisong <enobongitiaba988@gmail.com>
2023-08-14 23:28:00 -0700

    fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,19 +1,19 @@
 require_relative '../classes/genre'
 
 describe 'Testing Genre classes' do
-    before(:context) do
-        @genre = Genre.new('Song')
-    end
+  before(:context) do
+    @genre = Genre.new('Song')
+  end
 
   context 'Testing Genre instance and Method' do
     it 'Testing name instance' do
-        expect(@genre.name).to eq 'Song'
+      expect(@genre.name).to eq 'Song'
     end
 
     it 'Testing add_items method' do
-        @item = Item.new('2023-03-03')
-        @genre.add_items(@item)
-        expect(@genre.items).to include(@item)
+      @item = Item.new('2023-03-03')
+      @genre.add_items(@item)
+      expect(@genre.items).to include(@item)
     end
   end
-end 
+end
--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,23 +1,22 @@
-require_relative '../classes/music_album' 
+require_relative '../classes/music_album'
 
 describe 'Testing MusicAlbum class' do
-    before(:context) do
-        # (publish_date, on_spotify)
-        @musicalbum = MusicAlbum.new('2023-07-12', true)
+  before(:context) do
+    # (publish_date, on_spotify)
+    @musicalbum = MusicAlbum.new('2023-07-12', true)
+  end
+
+  context 'Testing MusicAlbum instance and Method' do
+    it 'Testing on spotify instance' do
+      expect(@musicalbum.on_spotify).to be true
     end
 
-    context 'Testing MusicAlbum instance and Method' do
-        it 'Testing on spotify instance' do
-            expect(@musicalbum.on_spotify).to be true
-        end
-
-        it 'Testing publish Date instance' do
-            expect(@musicalbum.publish_date).to eq '2023-07-12'
-        end        
-          
-
-        it 'Testing MusicAlbum class inheritance' do
-            expect(MusicAlbum).to be < Item
-        end
+    it 'Testing publish Date instance' do
+      expect(@musicalbum.publish_date).to eq '2023-07-12'
     end
+
+    it 'Testing MusicAlbum class inheritance' do
+      expect(MusicAlbum).to be < Item
+    end
+  end
 end
