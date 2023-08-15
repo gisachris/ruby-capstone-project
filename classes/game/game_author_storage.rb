0814c5f0ab8bc1048a76f88df48ec1aa146b5e89
gisa chris <gisachris24@gmail.com>
2023-08-15 12:49:24 -0200

    Add Read_From_Storage Method To Retrieve Stored Data

--- classes/game/game_author_storage.rb
+++ classes/game/game_author_storage.rb
@@ -23,6 +23,46 @@ class GameAuthorStorage
   end
 
   def read_from_storage
-    # Implement reading from JSON files and populating objects
+    games_data = read_json_data("#{DATA_DIRECTORY}/games.json")
+    authors_data = read_json_data("#{DATA_DIRECTORY}/authors.json")
+
+    populate_games(games_data)
+    populate_authors(authors_data)
+  end
+
+  private
+
+  def read_json_data(file_path)
+    return [] unless File.exist?(file_path)
+
+    json_data = File.read(file_path)
+    JSON.parse(json_data)
+  end
+
+  def populate_authors(authors_data)
+    authors_data.each do |author_data|
+      Author.new(
+        author_data['first_name'],
+        author_data['last_name'],
+        author_data['id']
+      )
+    end
+  end
+
+  def populate_games(games_data)
+    games_data.each do |game_data|
+      author = find_author(game_data['author_id'])
+      Game.new(
+        game_data['multiplayer'],
+        game_data['publish_date'],
+        game_data['last_played'],
+        author,
+        game_data['id']
+      )
+    end
+  end
+
+  def find_author(author_id)
+    Author.all_authors.find { |author| author.id == author_id }
   end
 end
