6349890da2c7b755e37dd72bed140096e36a28e0
gisa chris <gisachris24@gmail.com>
2023-08-16 13:27:53 -0200

    Fix Game And Authors Tests Related Linters

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -9,25 +9,25 @@ describe Author do
     Author.instance_variable_set(:@all_authors, []) # Reset all_authors before each example
   end
 
-  context "Testing the Author class" do
-    it "should create an author instance" do
+  context 'Testing the Author class' do
+    it 'should create an author instance' do
       expect(author_instance).to be_an_instance_of(Author)
     end
 
-    it "should populate the all_authors array" do
-      auth1 = Author.new('Sheldon', 'Cooper')
-      auth2 = Author.new('Howard', 'Wolowitz')
+    it 'should populate the all_authors array' do
+      Author.new('Sheldon', 'Cooper')
+      Author.new('Howard', 'Wolowitz')
       expect(Author.all_authors.length).to eq(2)
     end
 
-    it "should establish a relationship with the Items class" do
+    it 'should establish a relationship with the Items class' do
       item = Item.new('2010/03/04')
       author_instance.add_item(item)
       expect(author_instance.items).to include(item)
       expect(item.author).to eq(author_instance)
     end
 
-    it "should provide a hash representation" do
+    it 'should provide a hash representation' do
       expected_hash = {
         first_name: 'Sheldon',
         last_name: 'Cooper',
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,53 +3,64 @@ require_relative '../classes/item'
 require 'rspec'
 
 describe Game do
-  context "when testing the Game class" do
+  context 'when testing the Game class' do
     let(:fake_author) { double('author') }
 
     before(:each) do
       allow(fake_author).to receive(:first_name).and_return('Sheldon')
       allow(fake_author).to receive(:last_name).and_return('Cooper')
-
       Game.instance_variable_set(:@all_games, [])
     end
 
-    it "creates a game instance" do
+    it 'creates a game instance' do
       game1 = Game.new(true, '2010/04/03', '2019/03/04')
       expect(game1).to be_instance_of(Game)
     end
 
-    it "verifies that Game class inherits from Item class" do
+    it 'verifies that Game class inherits from Item class' do
       expect(Game.superclass).to eq(Item)
     end
 
-    it "populates the game array accordingly" do
-      game1 = Game.new(true, '2010/04/03', '2019/03/04')
-      game2 = Game.new(false, '2013/12/01', '2019/08/01')
+    it 'populates the game array accordingly' do
+      Game.new(true, '2010/04/03', '2019/03/04')
+      Game.new(false, '2013/12/01', '2019/08/01')
       game_array = Game.all_games
       expect(game_array.length).to eq(2)
     end
+  end
 
-    it "checks that last_played time variable is a date" do
+  context 'when checking date and author' do
+    let(:fake_author) { double('author') }
+
+    before(:each) do
+      allow(fake_author).to receive(:first_name).and_return('Sheldon')
+      allow(fake_author).to receive(:last_name).and_return('Cooper')
+      Game.instance_variable_set(:@all_games, [])
+    end
+
+    it 'checks that last_played time variable is a date' do
       game_instance = Game.new(true, '2010/04/03', '2019/03/04')
       expect(game_instance.last_played).to be_a(Date)
     end
 
-    it "initializes successfully when no author is given" do
+    it 'initializes successfully when no author is given' do
       game_instance = Game.new(true, '2010/04/03', '2019/03/04')
       expect(game_instance.author).to be_nil
     end
 
-    it "initializes successfully when an author is given" do
+    it 'initializes successfully when an author is given' do
       game_instance = Game.new(true, '2010/04/03', '2019/03/04', fake_author)
       expect(game_instance.author.first_name).to eq('Sheldon')
     end
+  end
 
-    it "archives a game when eligible" do
+  context 'when archiving a game' do
+    it 'archives a game when eligible' do
       game_instance = Game.new(true, '2010/04/03', '2010/04/05')
       expect(game_instance.can_be_archived?).to be_truthy
     end
 
-    it "does not archive a game when not eligible" do
+    it 'does not archive a game when not eligible' do
       game_instance = Game.new(true, '2010/04/03', (Date.today - 1).to_s)
       expect(game_instance.can_be_archived?).to be_falsey
     end
