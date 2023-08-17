020cbe88c227d3f2078987d756e17a7882835ed3
Enoisong <enobongitiaba988@gmail.com>
2023-08-17 01:56:12 -0700

    fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -3,7 +3,7 @@ require_relative '../classes/item'
 
 describe 'Testing Genre classes' do
   before(:context) do
-    @genre = Genre.new(name: 'Song')  
+    @genre = Genre.new(name: 'Song')
   end
 
   context 'Testing Genre instance and Method' do
@@ -15,7 +15,7 @@ describe 'Testing Genre classes' do
       @item = Item.new('2023-03-03')
       @genre.add_item(@item)
       expect(@genre.items).to include(@item)
-      expect(@item.genre).to eq(@genre)  
+      expect(@item.genre).to eq(@genre)
     end
   end
 end
